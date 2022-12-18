377958a4ef2586883ab415178fa0a5ddf6d1081e
Miriam Rosén <mirro457@student.liu.se>
2022-12-18 16:03:46 +0100

    Naive versin of part 2

--- 16/pipes.py
+++ 16/pipes.py
@@ -38,15 +38,18 @@ def path_len(src, dst, g, visited = []):
 def solve(cur, time_left, valves, g, path):
   if time_left > 1:
     total_flow = g[cur][0] * (time_left - 1)
+    best_path = []
     if len(valves) > 0:
       best = 0
       for next in valves:
         valve_copy = [v for v in valves if v != next]
-        flow = solve(next, time_left - path[(cur, next)] - 1, valve_copy, g, path)
+        flow, sub_path = solve(next, time_left - path[(cur, next)] - 1, valve_copy, g, path)
+        if flow > best:
+          best_path = sub_path
         best = max(best, flow)
       total_flow += best
-    return total_flow
-  return 0
+    return total_flow, [cur] + best_path
+  return 0, []
 
 ''' Part 1 '''
 if __name__ == "__main__" and len(argv) == 2:
@@ -59,5 +62,36 @@ if __name__ == "__main__" and len(argv) == 2:
       path[(v,u)] = path[(u,v)]
   
   non_zero_valves = [ v for v in g if g[v][0] > 0]
-  res = solve("AA", 31, non_zero_valves, g, path)
+  res, p = solve("AA", 31, non_zero_valves, g, path)
+  print(res, p)
+
+
+def solve_2(me, ele, t_m, t_e, valves, g, path):
+  if t_m > 1 or t_e > 1:
+    total_flow = max(g[me][0] * (t_m - 1), 0)
+    total_flow += max(g[ele][0] * (t_e - 1), 0)
+    if len(valves) > 0:
+      best = 0
+      for m_next in valves:
+        for e_next in valves:
+          if m_next != e_next:
+            valve_copy = [v for v in valves if v != m_next and v != e_next]
+            flow = solve_2(m_next, e_next, t_m - path[(me, m_next)] - 1, t_e - path[(ele, e_next)] - 1, valve_copy, g, path)
+            best = max(best, flow)
+      total_flow += best
+    return total_flow
+  return 0
+
+''' Part 2 '''
+if __name__ == "__main__" and len(argv) == 3 and argv[2] == '2':
+  g = read_input(open(argv[1]))
+
+  path = {}
+  for u in g:
+    for v in g:
+      path[(u,v)] = path_len(u, v, g)
+      path[(v,u)] = path[(u,v)]
+  
+  non_zero_valves = [ v for v in g if g[v][0] > 0]
+  res = solve_2("AA", "AA", 27, 27, non_zero_valves, g, path)
   print(res)
