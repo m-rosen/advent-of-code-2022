e3825b1b4792eb433c410ec1a2114ef32ebb6656
Miriam Rosén <mirro457@student.liu.se>
2022-12-23 14:58:28 +0100

    Day 16: Switched to faster shortest path algorithm

--- 16/pipes.py
+++ 16/pipes.py
@@ -1,49 +1,60 @@
 from sys import argv
+import time
 
 def read_input(f):
-  graph = {} # key : value ->  'AA' : (flow, neighbous[])
-  
+  i = 0
+  valve = {} # key : value -> name : (idx, capacity)
   for line in f:
     token = line.split()
-    valve = token[1]
-    flow = int(token[4].split('=')[1].strip(';'))
+    val = token[1]
+    cap = int(token[4].split('=')[1].strip(';'))
+    
+    valve[val] = (i, cap)
+    i += 1
+
+  f.seek(0)
+  edge = []
+  for line in f:
+    token = line.split()
+    val = token[1]
     neigbours = [n.strip(',') for n in token[9:]]
-
-    graph[valve] = (flow, neigbours)
+    
+    for n in neigbours:
+      edge.append((valve[val][0], valve[n][0]))
   
-  return graph
+  return valve, edge
 
 
-def path_len(src, dst, g, visited = []):
-  if src == dst:
-    return 0
+''' Floyd warshall's algorithm '''
+def shortest_paths(edges, nr_nodes):
+  inf =  2 * nr_nodes
+  dist = [[inf for _ in range(0, nr_nodes)] for _ in range(0, nr_nodes)]
 
-  if dst in g[src][1]:
-    return 1
-
-  inf = 2*len(g)
-  steps = inf
+  for (u,v) in edges:
+    dist[u][v] = 1
   
-  for n in g[src][1]:
-    if n not in visited:
-      p = path_len(n, dst, g, visited + [src])
-      if p == None:
-        continue
-      steps = min(p, steps)
+  for v in range(0, nr_nodes):
+    dist[v][v] = 0
   
-  if steps < inf:
-    return steps + 1
+  for k in range(0, nr_nodes):
+    for i in range(0, nr_nodes):
+        for j in range(0, nr_nodes):
+            if dist[i][j] > (dist[i][k] + dist[k][j]):
+              dist[i][j] = dist[i][k] + dist[k][j]
+  return dist
+
 
 ''' Find the optimal order to visit the valves '''
-def solve(cur, time_left, valves, g, path):
+def solve(cur, time_left, closed, valve, path):
   if time_left > 1:
-    total_flow = g[cur][0] * (time_left - 1)
+    total_flow = valve[cur][1] * (time_left - 1)
     best_path = []
-    if len(valves) > 0:
+    if len(closed) > 0:
       best = 0
-      for next in valves:
-        valve_copy = [v for v in valves if v != next]
-        flow, sub_path = solve(next, time_left - path[(cur, next)] - 1, valve_copy, g, path)
+      for next in closed:
+        closed_copy = [v for v in closed if v != next]
+        travel_time = path[valve[cur][0]][valve[next][0]]
+        flow, sub_path = solve(next, time_left - travel_time - 1, closed_copy, valve, path)
         if flow > best:
           best_path = sub_path
         best = max(best, flow)
@@ -51,47 +62,17 @@ def solve(cur, time_left, valves, g, path):
     return total_flow, [cur] + best_path
   return 0, []
 
+
 ''' Part 1 '''
 if __name__ == "__main__" and len(argv) == 2:
-  g = read_input(open(argv[1]))
+  start_t = time.time()
+  valve, edges = read_input(open(argv[1]))
 
-  path = {}
-  for u in g:
-    for v in g:
-      path[(u,v)] = path_len(u, v, g)
-      path[(v,u)] = path[(u,v)]
-  
-  non_zero_valves = [ v for v in g if g[v][0] > 0]
-  res, p = solve("AA", 31, non_zero_valves, g, path)
+  path = shortest_paths(edges, len(valve))
+  print("Paths found")
+  print(round(time.time() - start_t, 3), 's')
+
+  non_zero_valves = [ v for v, info in valve.items() if info[1] > 0]
+  res, p = solve("AA", 31, non_zero_valves, valve, path)
   print(res, p)
-
-
-def solve_2(me, ele, t_m, t_e, valves, g, path):
-  if t_m > 1 or t_e > 1:
-    total_flow = max(g[me][0] * (t_m - 1), 0)
-    total_flow += max(g[ele][0] * (t_e - 1), 0)
-    if len(valves) > 0:
-      best = 0
-      for m_next in valves:
-        for e_next in valves:
-          if m_next != e_next:
-            valve_copy = [v for v in valves if v != m_next and v != e_next]
-            flow = solve_2(m_next, e_next, t_m - path[(me, m_next)] - 1, t_e - path[(ele, e_next)] - 1, valve_copy, g, path)
-            best = max(best, flow)
-      total_flow += best
-    return total_flow
-  return 0
-
-''' Part 2 '''
-if __name__ == "__main__" and len(argv) == 3 and argv[2] == '2':
-  g = read_input(open(argv[1]))
-
-  path = {}
-  for u in g:
-    for v in g:
-      path[(u,v)] = path_len(u, v, g)
-      path[(v,u)] = path[(u,v)]
-  
-  non_zero_valves = [ v for v in g if g[v][0] > 0]
-  res = solve_2("AA", "AA", 27, 27, non_zero_valves, g, path)
-  print(res)
+  print(round(time.time() - start_t, 3), 's')
